a2f24b6f015778fa4f53fd3edd7bdad254ea8d88
heart14 <lwf14@qq.com>
2022-09-27 00:20:57 +0800

    导航栏路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -7,10 +7,10 @@ Vue.use(Router)
 import Layout from '@/layout'
 
 /* Router Modules */
-import componentsRouter from './modules/components'
-import chartsRouter from './modules/charts'
-import tableRouter from './modules/table'
-import nestedRouter from './modules/nested'
+// import componentsRouter from './modules/components'
+// import chartsRouter from './modules/charts'
+// import tableRouter from './modules/table'
+// import nestedRouter from './modules/nested'
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -74,37 +74,17 @@ export const constantRoutes = [
     path: '/',
     component: Layout,
     redirect: '/dashboard',
+    hidden: true,
     children: [
       {
         path: 'dashboard',
         component: () => import('@/views/dashboard/index'),
         name: 'Dashboard',
-        meta: { title: 'Dashboard', icon: 'dashboard', affix: true }
-      }
-    ]
-  },
-  {
-    path: '/documentation',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/documentation/index'),
-        name: 'Documentation',
-        meta: { title: 'Documentation', icon: 'documentation', affix: true }
-      }
-    ]
-  },
-  {
-    path: '/guide',
-    component: Layout,
-    redirect: '/guide/index',
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/guide/index'),
-        name: 'Guide',
-        meta: { title: 'Guide', icon: 'guide', noCache: true }
+        meta: {
+          title: '我们在街头',
+          icon: 'dashboard',
+          affix: true
+        }
       }
     ]
   },
@@ -118,7 +98,11 @@ export const constantRoutes = [
         path: 'index',
         component: () => import('@/views/profile/index'),
         name: 'Profile',
-        meta: { title: 'Profile', icon: 'user', noCache: true }
+        meta: {
+          title: '个人空间',
+          icon: 'user',
+          noCache: true
+        }
       }
     ]
   }
@@ -130,259 +114,253 @@ export const constantRoutes = [
  */
 export const asyncRoutes = [
   {
-    path: '/permission',
-    component: Layout,
-    redirect: '/permission/page',
-    alwaysShow: true, // will always show the root menu
-    name: 'Permission',
+    path: '/banner', // 路由地址
+    component: Layout, // 页面布局组件
+    redirect: '/banner/list', // 面包屑中重定向路由地址，若值为'noRedirect'则面包屑中不可点击
+    alwaysShow: true, // 不管children菜单个数是不是1，导航栏都会显示一个可折叠的菜单，若值为false，则children个数为1时，导航栏会直接显示children菜单
+    name: 'Banner', // 路由名字
     meta: {
-      title: 'Permission',
-      icon: 'lock',
-      roles: ['admin', 'editor'] // you can set roles in root nav
+      title: '街头大屏幕', // 导航栏中菜单名字
+      icon: 'el-icon-video-camera-solid', // 导航栏中菜单图标
+      roles: ['admin', 'mt'] // 配置权限
     },
     children: [
       {
-        path: 'page',
-        component: () => import('@/views/permission/page'),
-        name: 'PagePermission',
+        path: 'list',
+        component: () => import('@/views/banner/list'),
+        name: 'BannerList',
         meta: {
-          title: 'Page Permission',
-          roles: ['admin'] // or you can only set roles in sub nav
+          title: '视频放送'
+        }
+      }
+    ]
+  },
+  {
+    path: '/activity', // 路由地址
+    component: Layout, // 页面布局组件
+    redirect: '/activity/list', // 面包屑中重定向路由地址，若值为'noRedirect'则面包屑中不可点击
+    alwaysShow: true, // 不管children菜单个数是不是1，导航栏都会显示一个可折叠的菜单，若值为false，则children个数为1时，导航栏会直接显示children菜单
+    name: 'Activity', // 路由名字
+    meta: {
+      title: '街头公告栏', // 导航栏中菜单名字
+      icon: 'el-icon-data-analysis', // 导航栏中菜单图标
+      roles: ['admin', 'mt'] // 配置权限
+    },
+    children: [
+      {
+        path: 'list',
+        component: () => import('@/views/activity/list'),
+        name: 'ActivityList',
+        meta: {
+          title: '活动列表',
+          roles: ['admin', 'mt']
         }
       },
       {
-        path: 'directive',
-        component: () => import('@/views/permission/directive'),
-        name: 'DirectivePermission',
+        path: 'signup',
+        component: () => import('@/views/activity/signup'),
+        name: 'Signup',
         meta: {
-          title: 'Directive Permission'
-          // if do not set roles, means: this page does not require permission
+          title: '报名信息',
+          roles: ['admin', 'mt']
+        }
+      }
+    ]
+  },
+  {
+    path: '/about', // 路由地址
+    component: Layout, // 页面布局组件
+    redirect: '/about/info', // 面包屑中重定向路由地址，若值为'noRedirect'则面包屑中不可点击
+    alwaysShow: true, // 不管children菜单个数是不是1，导航栏都会显示一个可折叠的菜单，若值为false，则children个数为1时，导航栏会直接显示children菜单
+    name: 'About', // 路由名字
+    meta: {
+      title: '街头历史书', // 导航栏中菜单名字
+      icon: 'el-icon-notebook-2', // 导航栏中菜单图标
+      roles: ['admin', 'mt'] // 配置权限
+    },
+    children: [
+      {
+        path: 'info',
+        component: () => import('@/views/about/info'),
+        name: 'Info',
+        meta: {
+          title: '社团介绍',
+          roles: ['admin', 'mt']
+        }
+      },
+      {
+        path: 'history',
+        component: () => import('@/views/about/history'),
+        name: 'History',
+        meta: {
+          title: '发展历史',
+          roles: ['admin', 'mt']
+        }
+      }
+    ]
+  },
+  {
+    path: '/news', // 路由地址
+    component: Layout, // 页面布局组件
+    redirect: '/news/list', // 面包屑中重定向路由地址，若值为'noRedirect'则面包屑中不可点击
+    alwaysShow: true, // 不管children菜单个数是不是1，导航栏都会显示一个可折叠的菜单，若值为false，则children个数为1时，导航栏会直接显示children菜单
+    name: 'News', // 路由名字
+    meta: {
+      title: '街头黑板报', // 导航栏中菜单名字
+      icon: 'el-icon-s-grid', // 导航栏中菜单图标
+      roles: ['admin', 'mt'] // 配置权限
+    },
+    children: [
+      {
+        path: 'list',
+        component: () => import('@/views/news/list'),
+        name: 'NewsList',
+        meta: {
+          title: '活动图文列表',
+          roles: ['admin', 'mt']
+        }
+      },
+      {
+        path: 'create',
+        component: () => import('@/views/news/create'),
+        name: 'NewsCreate',
+        meta: {
+          title: '新建活动图文',
+          roles: ['admin', 'mt']
+        }
+      },
+      {
+        path: 'edit/:id(\\d+)',
+        component: () => import('@/views/news/edit'),
+        name: 'NewsEdit',
+        meta: {
+          title: '编辑活动图文',
+          noCache: true,
+          activeMenu: '/news/list'
+        },
+        hidden: true
+      }
+    ]
+  },
+  {
+    path: '/photo', // 路由地址
+    component: Layout, // 页面布局组件
+    redirect: '/photo/list', // 面包屑中重定向路由地址，若值为'noRedirect'则面包屑中不可点击
+    alwaysShow: true, // 不管children菜单个数是不是1，导航栏都会显示一个可折叠的菜单，若值为false，则children个数为1时，导航栏会直接显示children菜单
+    name: 'Photo', // 路由名字
+    meta: {
+      title: '街头画廊', // 导航栏中菜单名字
+      icon: 'el-icon-picture', // 导航栏中菜单图标
+      roles: ['admin', 'mt'] // 配置权限
+    },
+    children: [
+      {
+        path: 'list',
+        component: () => import('@/views/photo/list'),
+        name: 'PhotoList',
+        meta: {
+          title: '图片列表',
+          roles: ['admin', 'mt']
+        }
+      },
+      {
+        path: 'upload',
+        component: () => import('@/views/photo/upload'),
+        name: 'PhotoUpload',
+        meta: {
+          title: '图片上传',
+          roles: ['admin', 'mt']
+        }
+      }
+    ]
+  },
+  {
+    path: '/docs', // 路由地址
+    component: Layout, // 页面布局组件
+    redirect: '/docs/list', // 面包屑中重定向路由地址，若值为'noRedirect'则面包屑中不可点击
+    alwaysShow: true, // 不管children菜单个数是不是1，导航栏都会显示一个可折叠的菜单，若值为false，则children个数为1时，导航栏会直接显示children菜单
+    name: 'Docs', // 路由名字
+    meta: {
+      title: '街头档案馆', // 导航栏中菜单名字
+      icon: 'el-icon-wallet', // 导航栏中菜单图标
+      roles: ['admin', 'mt'] // 配置权限
+    },
+    children: [
+      {
+        path: 'list',
+        component: () => import('@/views/docs/list'),
+        name: 'DocsList',
+        meta: {
+          title: '社团资料',
+          roles: ['admin', 'mt']
+        }
+      }
+    ]
+  },
+  {
+    path: '/message', // 路由地址
+    component: Layout, // 页面布局组件
+    redirect: '/message/list', // 面包屑中重定向路由地址，若值为'noRedirect'则面包屑中不可点击
+    alwaysShow: true, // 不管children菜单个数是不是1，导航栏都会显示一个可折叠的菜单，若值为false，则children个数为1时，导航栏会直接显示children菜单
+    name: 'Message', // 路由名字
+    meta: {
+      title: '街头留言板', // 导航栏中菜单名字
+      icon: 'el-icon-message', // 导航栏中菜单图标
+      roles: ['admin', 'mt'] // 配置权限
+    },
+    children: [
+      {
+        path: 'list',
+        component: () => import('@/views/message/list'),
+        name: 'MessageList',
+        meta: {
+          title: '留言列表',
+          roles: ['admin', 'mt']
+        }
+      }
+    ]
+  },
+  {
+    path: '/user', // 路由地址
+    component: Layout, // 页面布局组件
+    redirect: '/user/list', // 面包屑中重定向路由地址，若值为'noRedirect'则面包屑中不可点击
+    alwaysShow: true, // 不管children菜单个数是不是1，导航栏都会显示一个可折叠的菜单，若值为false，则children个数为1时，导航栏会直接显示children菜单
+    name: 'User', // 路由名字
+    meta: {
+      title: '街头人事科', // 导航栏中菜单名字
+      icon: 'user', // 导航栏中菜单图标
+      roles: ['admin'] // 配置权限
+    },
+    children: [
+      {
+        path: 'list',
+        component: () => import('@/views/user/list'),
+        name: 'UserList',
+        meta: {
+          title: '用户列表',
+          roles: ['admin']
+        }
+      },
+      {
+        path: 'permission',
+        component: () => import('@/views/user/permission'),
+        name: 'Permission',
+        meta: {
+          title: '权限设置',
+          roles: ['admin']
         }
       },
       {
         path: 'role',
-        component: () => import('@/views/permission/role'),
-        name: 'RolePermission',
+        component: () => import('@/views/user/role'),
+        name: 'Role',
         meta: {
-          title: 'Role Permission',
+          title: '角色设置',
           roles: ['admin']
         }
       }
     ]
   },
-
-  {
-    path: '/icon',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/icons/index'),
-        name: 'Icons',
-        meta: { title: 'Icons', icon: 'icon', noCache: true }
-      }
-    ]
-  },
-
-  /** when your routing map is too long, you can split it into small modules **/
-  componentsRouter,
-  chartsRouter,
-  nestedRouter,
-  tableRouter,
-
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/list',
-    name: 'Example',
-    meta: {
-      title: 'Example',
-      icon: 'el-icon-s-help'
-    },
-    children: [
-      {
-        path: 'create',
-        component: () => import('@/views/example/create'),
-        name: 'CreateArticle',
-        meta: { title: 'Create Article', icon: 'edit' }
-      },
-      {
-        path: 'edit/:id(\\d+)',
-        component: () => import('@/views/example/edit'),
-        name: 'EditArticle',
-        meta: { title: 'Edit Article', noCache: true, activeMenu: '/example/list' },
-        hidden: true
-      },
-      {
-        path: 'list',
-        component: () => import('@/views/example/list'),
-        name: 'ArticleList',
-        meta: { title: 'Article List', icon: 'list' }
-      }
-    ]
-  },
-
-  {
-    path: '/tab',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/tab/index'),
-        name: 'Tab',
-        meta: { title: 'Tab', icon: 'tab' }
-      }
-    ]
-  },
-
-  {
-    path: '/error',
-    component: Layout,
-    redirect: 'noRedirect',
-    name: 'ErrorPages',
-    meta: {
-      title: 'Error Pages',
-      icon: '404'
-    },
-    children: [
-      {
-        path: '401',
-        component: () => import('@/views/error-page/401'),
-        name: 'Page401',
-        meta: { title: '401', noCache: true }
-      },
-      {
-        path: '404',
-        component: () => import('@/views/error-page/404'),
-        name: 'Page404',
-        meta: { title: '404', noCache: true }
-      }
-    ]
-  },
-
-  {
-    path: '/error-log',
-    component: Layout,
-    children: [
-      {
-        path: 'log',
-        component: () => import('@/views/error-log/index'),
-        name: 'ErrorLog',
-        meta: { title: 'Error Log', icon: 'bug' }
-      }
-    ]
-  },
-
-  {
-    path: '/excel',
-    component: Layout,
-    redirect: '/excel/export-excel',
-    name: 'Excel',
-    meta: {
-      title: 'Excel',
-      icon: 'excel'
-    },
-    children: [
-      {
-        path: 'export-excel',
-        component: () => import('@/views/excel/export-excel'),
-        name: 'ExportExcel',
-        meta: { title: 'Export Excel' }
-      },
-      {
-        path: 'export-selected-excel',
-        component: () => import('@/views/excel/select-excel'),
-        name: 'SelectExcel',
-        meta: { title: 'Export Selected' }
-      },
-      {
-        path: 'export-merge-header',
-        component: () => import('@/views/excel/merge-header'),
-        name: 'MergeHeader',
-        meta: { title: 'Merge Header' }
-      },
-      {
-        path: 'upload-excel',
-        component: () => import('@/views/excel/upload-excel'),
-        name: 'UploadExcel',
-        meta: { title: 'Upload Excel' }
-      }
-    ]
-  },
-
-  {
-    path: '/zip',
-    component: Layout,
-    redirect: '/zip/download',
-    alwaysShow: true,
-    name: 'Zip',
-    meta: { title: 'Zip', icon: 'zip' },
-    children: [
-      {
-        path: 'download',
-        component: () => import('@/views/zip/index'),
-        name: 'ExportZip',
-        meta: { title: 'Export Zip' }
-      }
-    ]
-  },
-
-  {
-    path: '/pdf',
-    component: Layout,
-    redirect: '/pdf/index',
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/pdf/index'),
-        name: 'PDF',
-        meta: { title: 'PDF', icon: 'pdf' }
-      }
-    ]
-  },
-  {
-    path: '/pdf/download',
-    component: () => import('@/views/pdf/download'),
-    hidden: true
-  },
-
-  {
-    path: '/theme',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/theme/index'),
-        name: 'Theme',
-        meta: { title: 'Theme', icon: 'theme' }
-      }
-    ]
-  },
-
-  {
-    path: '/clipboard',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/clipboard/index'),
-        name: 'ClipboardDemo',
-        meta: { title: 'Clipboard', icon: 'clipboard' }
-      }
-    ]
-  },
-
-  {
-    path: 'external-link',
-    component: Layout,
-    children: [
-      {
-        path: 'https://github.com/PanJiaChen/vue-element-admin',
-        meta: { title: 'External Link', icon: 'link' }
-      }
-    ]
-  },
-
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
 ]
