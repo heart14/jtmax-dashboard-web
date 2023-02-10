23ec8bc608409327dd0e8cff6e0c42fbe6705ea8
wfli <wfli@chinaums.com>
2023-02-10 11:36:46 +0800

    修改图片上传样式，上传完进行列表展示缩略图，点击查看预览

--- src/views/photo/upload.vue
+++ src/views/photo/upload.vue
@@ -1,15 +1,58 @@
 <template>
   <div class="components-container">
-    <el-upload class="upload-demo" drag action="http://127.0.0.1:28080/dashboard/photo/upload" multiple>
+    <el-upload
+      class="photo-upload"
+      drag
+      action="http://127.0.0.1:28080/dashboard/photo/upload"
+      multiple
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :file-list="fileList"
+      list-type="picture"
+      :headers="headers"
+    >
       <i class="el-icon-upload" />
       <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-      <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+      <div slot="tip" class="el-upload__tip">只能上传jpg/png/gif文件，且不超过500kb</div>
     </el-upload>
+    <el-dialog :visible.sync="dialogVisible">
+      <img width="100%" :src="dialogImageUrl" alt="">
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import { getToken } from '@/utils/auth'
+
 export default {
-  name: 'PhotoUpload'
+  name: 'PhotoUpload',
+  data() {
+    return {
+      headers: {},
+      dialogImageUrl: '',
+      dialogVisible: false,
+      fileList: []
+    }
+  },
+  created() {
+    this.initToken()
+  },
+  methods: {
+    initToken() {
+      this.headers = { Authorization: getToken() }
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url
+      this.dialogVisible = true
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList)
+    },
+    handlePreview(file) {
+      console.log(file)
+      this.dialogImageUrl = file.url
+      this.dialogVisible = true
+    }
+  }
 }
 </script>
