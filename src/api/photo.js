320e0b0a8b6ff240895925f59e2d04860f69cb94
wfli <wfli@chinaums.com>
2023-02-10 17:18:35 +0800

    图片列表页面分页查询、展示图片信息

--- src/api/photo.js
+++ src/api/photo.js
@@ -1,5 +1,13 @@
 import request from '@/utils/request'
 
+export function getPhotoList(data) {
+  return request({
+    url: '/photo/page_list',
+    method: 'post',
+    data
+  })
+}
+
 // 删除图片
 export function deletePhoto(id) {
   return request({
